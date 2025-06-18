48f390002ab24ce2a2f1ca21b9b8e6009a0b3017
Taras Pavlovskyi <taras.pavlovskyi@gmail.com>
2025-06-18 18:32:03 +0300

    add urls for home page

--- news_agency/urls.py
+++ news_agency/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from news_agency import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("", include("news.urls", namespace="news")),
+    path("accounts/", include("django.contrib.auth.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
