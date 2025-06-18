e086204cc1acb2f302058bf9889037fd0714b400
Taras Pavlovskyi <taras.pavlovskyi@gmail.com>
2025-06-18 18:13:04 +0300

    feat: add index view to display homepage

--- news/views.py
+++ news/views.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from news.models import Topic, Redactor, Newspaper
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_topics = Topic.objects.count()
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_topics": num_topics,
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "news/index.html", context=context)
