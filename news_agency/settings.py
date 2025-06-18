ad890b59252fa0299e117d6ef6fbc1df879e5d65
Taras Pavlovskyi <taras.pavlovskyi@gmail.com>
2025-06-18 18:26:14 +0300

    feat: add styles.css change settings for correct using

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -119,6 +119,10 @@ AUTH_USER_MODEL = "news.Redactor"
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
