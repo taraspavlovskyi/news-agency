615c0ead53d967697fc7bc7f3104887cc563ac01
Taras Pavlovskyi <taras.pavlovskyi@gmail.com>
2025-06-18 17:31:10 +0300

    feat: add Redactor model for news

--- news/models.py
+++ news/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
+
 
 class Topic(models.Model):
     name = models.CharField(
@@ -7,3 +10,17 @@ class Topic(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    def get_absolute_url(self):
+        return reverse("news:redactor_detail", kwargs={"pk": self.pk})
